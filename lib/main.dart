916c710cb5ea9e99c5a9ed31de21438701d19231
shinnkura <n.shinzo1010@icloud.com>
2023-06-25 14:46:03 +0900

    main.dartファイルを作成

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+//　新規アプリを起動した際のデフォルトのコード
+// コメントアウトしないで、書いて
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
